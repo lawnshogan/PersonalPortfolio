12c43cc2c09cff737cc0dd8e11c1055a76d8fbba
LawnShogan <shawnlogan.gis@gmail.com>
2023-03-09 13:47:37 -0700

    Index.html adjustments

--- assets/js/githubCalendar.js
+++ assets/js/githubCalendar.js
@@ -1,9 +1,16 @@
 GitHubCalendar(".calendar", "LawnShogan", {
   proxy: function(username) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.github.com/users/${username}/events/public`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        console.error('Error fetching GitHub events:', error);
+      });
   }
-}).then(function(response) {
-  return response.json();
 }).then(function(events) {
   console.log(events);
 }).catch(function(error) {
